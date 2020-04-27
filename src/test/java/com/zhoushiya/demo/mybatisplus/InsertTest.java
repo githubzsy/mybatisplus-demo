3ada22c39dee13b244c59610db9bc77018716d66
Zhoushiya <zhoushiya@126.com>
2020-04-27 13:59:19 +0800

    添加Insert单元测试

--- src/test/java/com/zhoushiya/demo/mybatisplus/InsertTest.java
+++ src/test/java/com/zhoushiya/demo/mybatisplus/InsertTest.java
@@ -9,18 +9,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleTest {
-
+public class InsertTest {
     @Resource
     private UserMapper userMapper;
 
     @Test
-    public void select(){
-        List<User> users= userMapper.selectList(null);
-        System.out.println(users);
+    public void Insert() {
+        User user=new User();
+        user.setName("李三");
+        user.setAge(31);
+        user.setManagerId(1088248166370832385l);
+        user.setCreateTime(LocalDateTime.now());
+        int rows=userMapper.insert(user);
+        System.out.println(rows);
     }
 }
---
+++ src/test/java/com/zhoushiya/demo/mybatisplus/SelectTest.java
@@ -0,0 +1,26 @@
+package com.zhoushiya.demo.mybatisplus;
+
+import com.zhoushiya.demo.dao.UserMapper;
+import com.zhoushiya.demo.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelectTest {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    public void select(){
+        List<User> users= userMapper.selectList(null);
+        System.out.println(users);
+    }
+}
